a4964c10f196d709e956786da2c017a3e0a80796
AH <andyhop@hotmail.co.uk>
2022-08-07 18:45:33 +0100

    Added config and resources

--- jsonParsing.py
+++ jsonParsing.py
@@ -2,18 +2,18 @@ import json
 
 courses = '{"name": "RahulShetty","languages": [ "Java", "Python"]}'
 
-#Loads method parse json string and it returns dictionary
+# Loads method parse json string and it returns dictionary
 dict_courses = json.loads(courses)
 print(type(dict_courses))
 print(dict_courses)
 print(dict_courses['name'])
-#get me the first language taught by trainer
+# get me the first language taught by trainer
 # list_language = dict_courses['languages']
 # print(type(list_language))
 # print(list_language[0])
 print(dict_courses['languages'][0])
 
-#****** Parse content present in Json file
+# ****** Parse content present in Json file
 with open('C:\\Users\\Owner\\Documents\\course.json') as f:
     data = json.load(f)
     print(data)
@@ -21,31 +21,14 @@ with open('C:\\Users\\Owner\\Documents\\course.json') as f:
     print(data['courses'][1]['title'])
     print(data['dashboard']['website'])
     print(type(data['dashboard']))
-#price of course 'RPA'
+    # price of course 'RPA'
     print(type(data['courses']))
     for course in data['courses']:
-        #print(course)
+        # print(course)
         if course['title'] == "RPA":
             print(course['price'])
-            assert course['price'] ==  45
+            assert course['price'] == 45
 
 with open('C:\\Users\\Owner\\Documents\\course1.json') as fi:
-    data2 =  json.load(fi)
+    data2 = json.load(fi)
     assert data == data2
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
---
+++ utilities/resources.py
@@ -0,0 +1,4 @@
+class ApiResources:
+    add_book = '/Library/Addbook.php'
+    delete_book = '/Library/DeleteBook.php'
+    get_book_author = ''
